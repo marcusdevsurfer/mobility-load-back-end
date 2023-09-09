4674939e07b53bc70808952df5b671517cd3493c
marcusdev99 <marcus.sea13@gmail.com>
2023-09-09 02:00:51 -0600

    feat : addition of SQL file, contains query test

---
+++ src/main/resources/static/data.sql
@@ -0,0 +1,7 @@
+--Insert units SQL to test
+INSERT INTO units (unit_name) VALUES ("Unidad 1");
+INSERT INTO units (unit_name) VALUES ("Unidad 2");
+INSERT INTO units (unit_name) VALUES ("Unidad 3");
+INSERT INTO units (unit_name) VALUES ("Unidad 4");
+INSERT INTO units (unit_name) VALUES ("Unidad 5");
+INSERT INTO units (unit_name) VALUES ("Unidad 6");
