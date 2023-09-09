01815163b6bf7bc64898348c1449d553f42d5af8
marcusdev99 <marcus.sea13@gmail.com>
2023-09-08 22:52:17 -0600

    refactor : addition of methods in controller class -retrieveUnits() -> GET -saveUnit() -> POST

--- src/main/java/com/devcoast/mobilityloadbackend/controller/UnitController.java
+++ src/main/java/com/devcoast/mobilityloadbackend/controller/UnitController.java
@@ -1,14 +1,31 @@
 package com.devcoast.mobilityloadbackend.controller;
 
-import org.springframework.stereotype.Controller;
+import com.devcoast.mobilityloadbackend.entity.Unit;
+import com.devcoast.mobilityloadbackend.service.UnitService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 public class UnitController {
 
-    @GetMapping("/")
-    public String firstController(){
-        return "Working...";
+    private final UnitService unitService;
+
+    public UnitController(UnitService unitService){
+        this.unitService = unitService;
+    }
+
+    @GetMapping("/units")
+    public ResponseEntity<List<Unit>> retrieveUnits(){
+        return new ResponseEntity<>(unitService.getAllUnits(),HttpStatus.OK);
+    }
+
+    @PostMapping("/units")
+    public ResponseEntity<Unit> saveUnit(@RequestBody Unit unit){
+        return new ResponseEntity<>(unitService.saveUnit(unit), HttpStatus.OK);
     }
 }
