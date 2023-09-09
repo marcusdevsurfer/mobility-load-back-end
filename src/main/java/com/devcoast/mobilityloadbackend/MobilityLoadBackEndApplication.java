5d81c3506dbc6c5e4e29333aef84e1956eeae745
marcusdev99 <marcus.sea13@gmail.com>
2023-09-08 21:43:04 -0600

    Initial commit

---
+++ src/main/java/com/devcoast/mobilityloadbackend/MobilityLoadBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.devcoast.mobilityloadbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilityLoadBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilityLoadBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcoast/mobilityloadbackend/MobilityLoadBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoast.mobilityloadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilityLoadBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
