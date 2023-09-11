209b37d4e476c15e6c82ec8b54d09224cc16492d
marcusdev99 <marcus.sea13@gmail.com>
2023-09-11 13:21:33 -0600

    refactor : addition of findByCarrierId method in repository

--- src/main/java/com/devcoast/mobilityloadbackend/repository/UnitRepository.java
+++ src/main/java/com/devcoast/mobilityloadbackend/repository/UnitRepository.java
@@ -4,6 +4,9 @@ import com.devcoast.mobilityloadbackend.entity.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UnitRepository extends JpaRepository<Unit,Long> {
+    List<Unit> findByCarrierId(Long carrierId);
 }
