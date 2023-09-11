25512e896f2f8876a15ff668f1f9a04d12b773e5
marcusdev99 <marcus.sea13@gmail.com>
2023-09-11 14:30:22 -0600

    feat : addition of findByEmail method to repository

--- src/main/java/com/devcoast/mobilityloadbackend/repository/CarrierRepository.java
+++ src/main/java/com/devcoast/mobilityloadbackend/repository/CarrierRepository.java
@@ -4,6 +4,10 @@ import com.devcoast.mobilityloadbackend.entity.Carrier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CarrierRepository extends JpaRepository<Carrier,Long> {
+    //TODO : Create method to find Carrier by email
+    Optional<Carrier> findByEmail(String email);
 }
